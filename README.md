f2ac5003b8ffea7ab489b0ba3d2d9df53593bb0e
liuhangbiao <liuhangbiaoo@gmail.com>
2017-04-19 14:02:41 +0800

    uploade

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # toolbox
 基于npm 构建的工具箱(toolbox)
+
+
--- src/js/test.js
+++ src/js/test.js
@@ -1,6 +1,29 @@
-/**
- * @return {[type]}
- */
-function aa(){
+document.getElementsByTagName('div')
 
-}
+var a=new Array();
+var as=[ 6, 4212, 3, 124, 12 ]
+
+// /**
+//  * @return {[type]}
+//  */
+// function aa() {
+
+// 	var zxcz = "zxczxc";
+// 	var c = 132432 c;
+
+// 	console.log('test');
+
+// }
+
+// document.getElementsByTagName('')
+
+// p
+
+// POST http: //192.168.13.160:8123/domwebserver/login
+// 	Content - type: application / json;
+// charset = UTF - 8
+// POST_BODY: loginName = 21232 f297a57a5a743894a0e4a801fc3 & amp;
+// password = 21232 F297A57A5A743894A0E4A801FC3
+
+
+// // {loginName:"21232f297a57a5a743894a0e4a801fc3",password:"21232F297A57A5A743894A0E4A801FC3"}
